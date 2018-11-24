7f8f6fcacf5ae203ae72526e5b2dbb8dabd99ac6
maneeshapaladugu <41922387+maneeshapaladugu@users.noreply.github.com>
2018-11-24 19:34:58 +0530

    Add files via upload

---
+++ c_preprocessor.c
@@ -0,0 +1,11 @@
+void macro_replace(char*);
+void rem_cmnts(char*);
+void header_incl(char*);
+
+int main(int argc,char **argv)
+{
+  //macro_replace(argv[1]);
+  header_incl(argv[1]);
+  rem_cmnts("temp1.c");
+return 0;
+}
